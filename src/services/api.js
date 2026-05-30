9309e547ebef3faee89f85cbe646950b067d0f2b
Wannn Sion <wannnsion95@gmail.com>
2026-05-30 16:42:45 +0700

    update API

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "55742c41b8314d37d173c2cdbedfc728";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
